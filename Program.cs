6ee7e743d131cfddd5dc648396810ef44807cb14
Abdülsamet YILMAZ <mr.asmtylmz@gmail.com>
2023-03-07 18:17:31 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Hangi sayıdan başlamak istediğinizi yazınız : ");
+int baslangıc = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-----------------------------------------------");
+Console.Write("Hangi sayıya kadar sıralı toplama işlemi yapmak istediğinizi yazınız : ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-----------------------------------------------");
+
+int toplam = 0;
+
+
+
+for (int i = baslangıc; i <= sayi; i++)
+{
+    toplam += i;
+
+}
+Console.WriteLine($"Sonuç : {toplam}");
